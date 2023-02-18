1177e213f8d72486425f80090c730a56182eaa70
R-salton <ringusa22@gmail.com>
2023-02-18 16:09:59 +0000

    Final project

--- app.js
+++ app.js
@@ -1,16 +1,35 @@
 //jshint esversion:6
 require('dotenv').config();
+const md5 = require('md5')
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
-const encrypt = require('mongoose-encryption');
+const session = require('express-session');
+const passport = require('passport');
+const passportLocal = require('passport-local');
+const passportLocalMongoose = require('passport-local-mongoose');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require('mongoose-findorcreate');
 
+// const encrypt = require('mongoose-encryption');
+// const bcrypt = require('bcrypt')
+// const saltRounds = 12;
 
 const app = express();
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(session({
+    secret: 'ourSecrete',
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+
+app.use(passport.session());
 
 //======= MONGODB CONNECTION ========= //
 mongoose.set('strictQuery', true)
@@ -25,20 +44,66 @@ mongoose.connect('mongodb://127.0.0.1:27017/users', (err) =>{
 // USER SCHEMA
 const userSchema = new mongoose.Schema({
     email: String,
-    password: String
+    password: String,
+    googleId: String,
+    secret: String
 });
-const secret = process.env.SECRET;
-userSchema.plugin(encrypt, {secret: secret, encryptedFields: ['password']});
+
+userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
+// ENCRYPTING OUR DATA WITH Mongoose-encryption
+
+// const secret = process.env.SECRET;
+// userSchema.plugin(encrypt, {secret: secret, encryptedFields: ['password']});
 
 // USER MODEL
 const Users = mongoose.model('User', userSchema);
 
+passport.use(Users.createStrategy());
+// passport.serializeUser(Users.serializeUser());
+// passport.deserializeUser(Users.deserializeUser());
 
+passport.serializeUser(function(Users, done) {
+    done(null, Users);
+  });
+  
+  passport.deserializeUser(function(id, done) {
+    Users.findById(id, function(err, user){
+
+        done(err, user);
+    })
+  });
+
+
+  // GOOGLE AUTH
+
+  passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secretes"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    
+    Users.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
 
 app.get('/',(req,res) =>{
     res.render('home')
 })
 
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ['profile'] }));
+
+
+  app.get('/auth/google/secretes', 
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/secrets');
+  });
 app.get('/register',(req,res) =>{
     res.render('register')
 })
@@ -47,57 +112,101 @@ app.get('/login',(req,res) =>{
     res.render('login')
 })
 
+app.get('/secrets', function(req,res){
+   
+    Users.find({secret: {$ne: null}}, function(err, foundUsers){
+        if(err){
+            console.log(err)
+        }
+        else{
+            res.render('secrets',{foundUsers: foundUsers})
+        }
+    })
+})
+
+// GET SUBMIT REQUEST FRO USER
+
+app.get('/submit', function(req, res)
+{
+     if(req.isAuthenticated()){
+        res.render("submit")
+     } else(
+        res.redirect('/login')
+     )
+
+})
+
+app.post('/submit', function(req, res){
+    const secret = req.body.secret;
+    Users.findById(req.user._id, function(err, foundUser)
+    {
+        if(err)
+        { 
+            console.log(err)
+        }
+        else
+        {
+            foundUser.secret = secret;
+            foundUser.save();
+            res.redirect('/secrets')
+        }
+    }
+    )
+    // Users.findById(req.)
+})
 
 // REGISTER USER
 app.post('/register', (req, res) =>{
 
-const username = req.body.username;
-const password = req.body.password;
- Users.findOne({email: username}, function(err, foundUser){
-    if(err){
-        console.log(err);
-    } else if(foundUser){
-       console.log("user exist");
-       res.render('register'); 
-    }
-    else{
+    Users.register({username: req.body.username}, req.body.password, function(err, registeredUser){
+        if(err){
+            console.log(err);
+            res.redirect('/register');
+        } else{
+            passport.authenticate('local')(req, res, function(){
+                res.redirect('/secrets');
+            });
+        }
+    })
 
-        const user = new Users({
-            email: username,
-            password: password
-        });
-        user.save(function(err){
-            if(err){
-                console.log(err);
-            } else{
-                res.render('secrets')
-            }
-        })
-    }
- })
-            
-           
+
 });
+
+            
+        
 
 // USER LOGIN
 
 app.post('/login', function(req, res){
     const username = req.body.username;
     const password = req.body.password;
-
-    Users.findOne({email: username}, function(err, foundUser){
+    const newUser = new Users({
+        email: username,
+        password: password
+    });
+    req.login(newUser, function(err){
         if(err){
-            console.log(err);
+            console.log(err)
         } else{
-            if(foundUser){
-                if(foundUser.password === password){
-                    res.render('secrets');
-                }
-            }
+            passport.authenticate('local')(req, res, function(){
+                res.redirect('secrets')
+            })
         }
     })
+
+    
 });
 
+
+// LOGOUT USER ROUTE
+app.get('/logout', function(req, res){
+    req.logout(function(err){
+        if(err){
+            console.log(err)
+        }
+    });
+    res.redirect('/')
+;})
 
 app.listen(3000,function(){
     console.log('====================================');
